00d51242ec49c4ea8fe5a7a82c032091ec6149fa
Paweł Grabiński <pawelrgrabinski@gmail.com>
2017-12-05 00:17:44 +0100

    L2 final

--- Lista2/L1E4.cpp
+++ Lista2/L1E4.cpp
@@ -10,27 +10,31 @@ double expectN(int N){
   gsl_rng * r = gsl_rng_alloc (gsl_rng_mt19937);
   int counter = 0;
   int counters[inf];
+  for(int i=0;i<inf;i++){
+    counters[i]=0;
+  }
   int rest = 0;
   double productTemp = 0;
   double productN = 0;
   for(int i = 0; i<N; i++){
     counter = 0;
-    productTemp = 0;
+    productTemp = 1;
     while (productTemp > e3){
       productTemp *=  gsl_rng_uniform (r);
       counter++;
     }
+    counter--;
     productN += counter;
     if(counter < inf) {
 		counters[counter]++;
-	} else {
+	  } else {
 		rest++;
-	}
+	   }
   }
   for(int i=1; i < inf; i++) {
-    cout << "For N= " << i << " the probability is: " << (counters[i]/ N) << "\n";
+    cout << "For N=" << i << " the probability is: " << ((double) counters[i]/ (double) N) << "\n";
   }
-	cout << "For N>=100 the probability is: " << (rest/N) << "\n";
+	cout << "For N>19 the probability is: " << ((double) rest/(double) N) << "\n";
 
   return productN/N;
 }
@@ -38,6 +42,7 @@ double expectN(int N){
 
 int main() {
   gsl_rng_env_setup();
-  cout << expectN(1000);
+  //cout << "Exp(-3) " << e3 << "\n";
+  cout << "The expectation value is: " << expectN(1000000000) << "\n";
   return 0;
 }
---
+++ Projekt1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+#include <gsl/gsl_rng.h>
+
+
+int main() {
+
+  return 0;
+}
