5a254b195e5169607f153b2ac88254452821a163
Paweł Grabiński <pawelrgrabinski@gmail.com>
2017-12-06 00:58:13 +0100

    Almost ready sketch

--- Projekt1/main.cpp
+++ Projekt1/main.cpp
@@ -4,10 +4,16 @@
 
 // Size of the square lattice (LxL)
 const int latticeSize = 16;
-// Array of the LxL array values -1(down) or 1(up)
+// Array of the LxL lattice values -1(down) or 1(up)
 int latticeArray[latticeSize][latticeSize];
+// Array of the LxL flipping helpers
+int helpersArray[latticeSize][latticeSize];
 // Coupling constant
 const int couplingJ = -1;
+// RNG
+gsl_rng * s = gsl_rng_alloc (gsl_rng_mt19937);
+// Temperature
+double temperature = 1.0;
 
 // Imposses cyclic boundary conditions on x coordinate
 int cyclicBoundariesX(int xPosition, int move){
@@ -54,6 +60,13 @@ double spinFlipProbability(double temperature, int xPosition, int yPosition){
   return 1.0 - exp(-2 * spinInteractionSum / temperature);
 }
 
+void clearHelpers(){
+  for(int i=0;i<latticeSize;i++){
+    for(int j=0;j<latticeSize;j++){
+      helpersArray[i][j]=0;
+    }
+  }
+}
 
 // Sets values in the lattice for -1(down) or 1(up)
 void shuffleLattice(){
@@ -68,15 +81,38 @@ void shuffleLattice(){
 
 void addConnection(int xPositionCurrent, int yPositionCurrent,int xPositionPrevious, int yPositionPrevious){
   int currentSpin = latticeArray[xPositionCurrent][yPositionCurrent];
-  if( currentSpin != latticeArray[xPositionPrevious][yPositionPrevious] ) return;
-  
+  if( xPositionPrevious!=-10 && (currentSpin != latticeArray[xPositionPrevious][yPositionPrevious] || helpersArray[xPositionCurrent][yPositionCurrent]==1)  ) return;
+  if(gsl_rng_uniform(s) < spinFlipProbability(temperature,xPositionCurrent,yPositionCurrent) ){
+    helpersArray[xPositionCurrent][yPositionCurrent]=1;
+    addConnection(cyclicBoundariesX(xPositionCurrent,-1),yPositionCurrent,xPositionCurrent,yPositionCurrent);
+    addConnection(cyclicBoundariesX(xPositionCurrent,1),yPositionCurrent,xPositionCurrent,yPositionCurrent);
+    addConnection(xPositionCurrent,cyclicBoundariesY(yPositionCurrent,-1),xPositionCurrent,yPositionCurrent);
+    addConnection(xPositionCurrent,cyclicBoundariesY(yPositionCurrent,1),xPositionCurrent,yPositionCurrent);
+  }
 }
 
+
+void timeControl(){
+  // Number of time steps
+  int N = 1000;
+  // Temp variables for (x,y) of the begining node
+  int tempX;
+  int tempY;
+  for(int i=0;i<N;i++){
+    tempX = (int) (latticeSize * gsl_rng_uniform(s));
+    tempY = (int) (latticeSize * gsl_rng_uniform(s));
+    addConnection(tempX,tempY,-10,-10);
+
+
+    clearHelpers();
+  }
+}
 
 
 int main() {
   gsl_rng_env_setup();
   shuffleLattice();
-
+  clearHelpers();
+  timeControl();
   return 0;
 }
